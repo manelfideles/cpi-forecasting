c3b7b6720d57b7a6afa42d949d41ba3aa94e7821
Manuel Fideles <manuelfideles77@gmail.com>
2022-10-16 14:59:36 +0100

    First commit: Dataset I/O, sanitizing and cleaning

---
+++ src/scripts/dataset_io.py
@@ -0,0 +1,49 @@
+from os import path, rename
+import numpy as np
+import pandas as pd
+
+
+def getDatasetFilePath(dataDir: str, countryName: str, fallback: str) -> str:
+    if path.isfile(f'{dataDir}/{countryName}.pkl'):
+        return f'{dataDir}/{countryName}.pkl'
+    return f'{dataDir}/{fallback}.csv'
+
+
+def sanitizeDataset(dataDir: str, settings: 'dict[str, any]') -> None:
+    countryName: str = settings['countryName']
+    indicator: str = settings['indicator']
+    relevantCols: 'tuple[str]' = tuple(
+        [str(elem) for elem in range(1990, 2023)]
+        + ['Country Name', 'Indicator Code', 'Attribute']
+    )
+    df: pd.DataFrame = pd \
+        .read_csv(
+            settings['filePath'],
+            low_memory=False,
+            index_col=[0]) \
+        .reset_index() \
+        .query("`Country Name` == @countryName & `Indicator Code` == @indicator & Attribute == 'Value'") \
+        .drop(['Country Code', 'Country Name', 'Indicator Code', 'Attribute'], axis=1)
+    df: pd.DataFrame = df \
+        .loc[:, df.columns.str.startswith(relevantCols)] \
+        .transpose() \
+        .rename(columns={8222: 'cpi'}) \
+        .reset_index() \
+        .rename(columns={'index': 'ym'})
+
+    df[['year', 'month']] = df['ym'].str.split('M', n=1, expand=True)
+    df = df.drop(['ym'], axis=1)
+
+    # save to disk in pickle for better performance
+    pd.to_pickle(df, f'{dataDir}/{countryName}.pkl')
+
+
+def getDataset(dataDir: str, settings: 'dict[str, any]') -> pd.DataFrame:
+    print('Reading from ' + settings['filePath'] + ' ...')
+    if ('pkl' not in settings['filePath']):
+        sanitizeDataset(dataDir, settings)
+    df: pd.DataFrame = pd.read_pickle(
+        f'{dataDir}/' + settings['countryName'] + '.pkl'
+    )
+    print('Done!')
+    return df
